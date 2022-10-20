9107039b8bd60c21ddb7a72773b697289cb42d16
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-10-20 14:14:06 +0900

    사용하지 않는 기능 제거

--- code-gen-web/src/App.tsx
+++ code-gen-web/src/App.tsx
@@ -1,20 +1,10 @@
-import http from './http';
-import { SWRConfig } from 'swr'
-import Organizations from './Organizations'
-import WeatherForecast from './WeatherForecast'
 import CodeGen from './CodeGen';
-
-const fetcher = (url: string) => http.get(url).then(res => res.data);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <CodeGen />
-        {/* <SWRConfig value={{ fetcher }}>
-          <Organizations />
-          <WeatherForecast />
-        </SWRConfig> */}
       </header>
     </div>
   )
--- code-gen-web/src/main.tsx
+++ code-gen-web/src/main.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { setCreateHttp } from './api'
 import App from './App'
 import './index.css'
-import axios from 'axios';
-
-setCreateHttp(() => axios.create({ baseURL: 'http://localhost:5000/' }));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
