82c5033f37c82eb99115c8eedd60807195475507
Hyeonguk Ryu <ryu@hyeonguk.com>
2025-09-18 13:19:13 +0900

    CODEGEN_CLI_WATCH 동작 개선

--- code-gen-cli/index.ts
+++ code-gen-cli/index.ts
@@ -1,12 +1,18 @@
 import { existsSync } from 'fs';
-import { mkdir, rm, writeFile } from 'fs/promises';
+import { mkdir, opendir, readFile, rm, writeFile } from 'fs/promises';
 import path from 'path';
 
-const ServerRoot = process.argv[2].replace(/\/$/, '');
+const ServerRoot = process.argv[2]?.replace(/\/$/, '');
 if (!ServerRoot) {
     console.error('Usage: node update-api.js <server-root>');
+    console.error('Environment variables:');
+    console.error('    CODEGEN_CLI_WATCH');
+    console.error('        Set to "Y" to enable watch mode (default: off)');
+    console.error('    CODEGEN_CLI_WATCH_INTERVAL');
+    console.error('        Interval in milliseconds for watch mode (default: 1000)');
     process.exit(1);
 }
+
 const Watch = process.env['CODEGEN_CLI_WATCH'] === 'Y';
 const Interval = process.env['CODEGEN_CLI_WATCH_INTERVAL'] ?
     parseInt(process.env['CODEGEN_CLI_WATCH_INTERVAL']) : 1000;
@@ -85,8 +91,61 @@ async function writeFiles(files: WriteFileRequest[], codePath: string): Promise<
 
     for (const file of files) {
         const filePath = path.join(codePath, file.fileName);
+
+        const operation = await (async () => {
+            if (!existsSync(filePath)) {
+                return { code: 'create', message: 'Created' } as const;
+            }
+            const content = await readFile(filePath, 'utf-8');
+            if (content !== file.content) {
+                return { code: 'update', message: 'Updated' } as const;
+            }
+            return { code: 'none', message: null } as const;
+        })();
+
+        if (operation.code === 'none') {
+            continue;
+        }
         await writeFile(filePath, file.content);
+        writeLog(`${operation.message}: ${filePath}`);
     }
+}
+
+async function clean(dir: string, files: string[]): Promise<void> {
+    if (!Watch) {
+        await rm(dir, { recursive: true, force: false });
+        writeLog(`Removed: ${dir}`);
+        return;
+    }
+
+    const entries = await opendir(dir);
+    for await (const entry of entries) {
+        if (!entry.isFile()) {
+            throw new Error(`Unexpected directory entry: ${entry.name}`);
+        }
+        if (!files.includes(entry.name)) {
+            const filePath = path.join(dir, entry.name);
+            await rm(filePath);
+            writeLog(`Removed: ${filePath}`);
+        }
+    }
+}
+
+function getLocalTimestamp(): string {
+    const now = new Date();
+
+    const year = now.getFullYear().toString().padStart(4, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const hour = now.getHours().toString().padStart(2, '0');
+    const minute = now.getMinutes().toString().padStart(2, '0');
+    const second = now.getSeconds().toString().padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
+function writeLog(message: string) {
+    console.log(`[${getLocalTimestamp()}] ${message}`);
 }
 
 async function main() {
@@ -104,19 +163,31 @@ async function main() {
     for (; ;) {
         try {
             const clientCode = await getCode(true, '../client.config');
-            validateCode(clientCode);
             const serverCode = await getCode(false, '../server.config');
+            if (Watch && (clientCode === null || serverCode === null)) {
+                writeLog('Failed to get code');
+                continue;
+            }
+
+            validateCode(clientCode);
             validateCode(serverCode);
 
             if (prevClientCode !== clientCode || prevServerCode !== serverCode) {
+                writeLog('Updating API code');
+
                 const clientFiles = splitCode(clientCode);
                 const serverFiles = splitCode(serverCode);
 
-                await rm('src/api/client', { recursive: true });
-                await rm('src/api/server', { recursive: true });
+                await clean('src/api/client', clientFiles.map(f => f.fileName));
+                await clean('src/api/server', serverFiles.map(f => f.fileName));
 
                 await writeFiles(clientFiles, ClientRoot);
                 await writeFiles(serverFiles, ServerRoot);
+
+                prevClientCode = clientCode;
+                prevServerCode = serverCode;
+
+                writeLog('Updated API code');
             }
 
             if (!Watch) {
@@ -127,9 +198,11 @@ async function main() {
             if (!Watch) {
                 process.exit(1);
             }
+        } finally {
+            if (Watch) {
+                await new Promise(resolve => setTimeout(resolve, Interval));
+            }
         }
-
-        await new Promise(resolve => setTimeout(resolve, Interval));
     }
 }
 main();
